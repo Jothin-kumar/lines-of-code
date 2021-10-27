0d619bf88298437073ac50dba9e9f2ed9e1ac704
Jothin-kumar <bjothinphysics@gmail.com>
2021-10-27 23:18:42 +0530

    Create github_user_stats.py

---
+++ github_user_stats.py
@@ -0,0 +1,27 @@
+from github import Github, Repository
+
+github = Github('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')
+user = github.get_user()
+
+
+class ContributedRepository:
+    def __init__(self, contributed_to: Repository):
+        self.total_lines_of_addition_in_contribution = 0
+        self.total_lines_of_deletion_in_contribution = 0
+        self.name = contributed_to.full_name
+        for contribution in contributed_to.get_stats_contributors():
+            if contribution.author.id == user.id:
+                for week in contribution.weeks:
+                    self.total_lines_of_addition_in_contribution += week.a
+                    self.total_lines_of_deletion_in_contribution += week.d
+        if not any([self.total_lines_of_addition_in_contribution, self.total_lines_of_deletion_in_contribution]):
+            raise ValueError('User have not contributed to this repository.')
+
+
+contributed_repos = []
+for repo in user.get_repos():
+    if repo.fork:
+        try:
+            contributed_repos.append(ContributedRepository(contributed_to=repo.parent))
+        except ValueError:
+            pass
