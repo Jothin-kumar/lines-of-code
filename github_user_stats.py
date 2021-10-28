13138f83fcdd3a3c7109c2cc7978f2d49cd19314
Jothin-kumar <bjothinphysics@gmail.com>
2021-10-28 09:23:52 +0530

    Own repos added

--- github_user_stats.py
+++ github_user_stats.py
@@ -18,10 +18,25 @@ class ContributedRepository:
             raise ValueError('User have not contributed to this repository.')
 
 
+class OwnedRepository:
+    def __init__(self, owned_repository: Repository):
+        self.total_lines_of_addition = 0
+        self.total_lines_of_deletion = 0
+        self.name = owned_repository.name
+        for contribution in owned_repository.get_stats_contributors():
+            if contribution.author.id == user.id:
+                for week in contribution.weeks:
+                    self.total_lines_of_addition += week.a
+                    self.total_lines_of_deletion += week.d
+
+
 contributed_repos = []
+own_repos = []
 for repo in user.get_repos():
     if repo.fork:
         try:
             contributed_repos.append(ContributedRepository(contributed_to=repo.parent))
         except ValueError:
             pass
+    else:
+        own_repos.append(OwnedRepository(owned_repository=repo))
